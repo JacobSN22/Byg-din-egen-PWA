d0038292a253c7c76b24ade05a00a2232212057d
Jacob <jacobsimoninielsen@gmail.com>
2023-04-25 15:00:40 +0200

    Update

---
+++ js/firestore.config.js
@@ -0,0 +1,17 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBWWh-XMq8qn0fnLFHOC_wJqsz9zTQtnWM",
+    authDomain: "build-own-pwa.firebaseapp.com",
+    projectId: "build-own-pwa",
+    storageBucket: "build-own-pwa.appspot.com",
+    messagingSenderId: "879725177342",
+    appId: "1:879725177342:web:58e33d0b4b13152f24b8af"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
